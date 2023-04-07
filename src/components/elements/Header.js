cab284deaff77834418f3969cf5b1ede71e025a4
Felipe Malicheski <felipemalicheski@gmail.com>
2023-04-07 15:12:59 -0300

    Header responsivo

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -3,8 +3,11 @@ import style from "./Header.module.css";
 import AddIcon from "@mui/icons-material/Add";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import GridViewIcon from "@mui/icons-material/GridView";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
-import { Menu, MenuItem, MenuList } from "@mui/material";
+import { Menu, MenuItem, MenuList, Drawer } from "@mui/material";
 
 function Header() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -12,13 +15,39 @@ function Header() {
     setAnchorEl(!anchorEl ? event.currentTarget : null);
   };
 
-  const handleMouseLeave = () => {
-    setAnchorEl(null);
-  };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const [state, setState] = useState({ left: false });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+  const list = (anchor) => (
+    <div
+      role="presentation"
+      className={style["list"]}
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <ul>
+        <li><a>Gerenciar publicações</a></li>
+        <li><a>Minhas publicações</a></li>
+        <li><a>Perfil</a></li>
+        <li><a>Favoritos</a></li>
+        <li><a>Sair</a></li>
+      </ul>
+    </div>
+  );
+
   return (
     <header className={style["header"]}>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
@@ -33,30 +62,38 @@ function Header() {
       ></link>
 
       <nav>
-        <div>
+        <div className={style['logo-menu']}>
+          <button className={style['menu-button']} onClick={toggleDrawer("left", true)}>
+            <MenuIcon ></MenuIcon>
+          </button>
           <a href="/">
-            <img src={require("./logo_black.png")}></img>
+            <img src={require("./logo_black_2.png")}></img>
           </a>
         </div>
-        <div className={style['nav']}>
+        <div className={style["nav"]}>
           <div>
-            <a href="/">Gerenciar publicações</a>
+            <a className={style["align-icons"]} href="/">
+              <ContentPasteIcon sx={{ fontSize: "2.25rem" }}></ContentPasteIcon>
+              Gerenciar publicações
+            </a>
           </div>
           <div>
-            <a href="/login">Minhas publicações</a>
+            <a className={style["align-icons"]} href="/login">
+              <GridViewIcon sx={{ fontSize: "2.25rem" }}></GridViewIcon>Minhas
+              publicações
+            </a>
           </div>
           <div>
             <button
               className={style["user-menu"]}
               aria-controls="menu"
               aria-haspopup="true"
-              onMouseOver={handleMouseEnter} // adicionando o evento onMouseEnter
-
+              onClick={handleMouseEnter} // adicionando o evento onMouseEnter
             >
               <AccountCircleIcon
                 sx={{ fontSize: "2.25rem" }}
               ></AccountCircleIcon>{" "}
-              Roberto Carlos <ArrowDropDownIcon></ArrowDropDownIcon>
+              Roberto<ArrowDropDownIcon></ArrowDropDownIcon>
             </button>
             <Menu
               className={style["menu"]}
@@ -78,6 +115,15 @@ function Header() {
           </div>
         </div>
       </nav>
+      <Drawer
+        className={style["drawer"]}
+        anchor="left"
+        open={state["left"]}
+        onClose={toggleDrawer("left", false)}
+      >
+        {" "}
+        {list("left")}
+      </Drawer>
     </header>
   );
 }
