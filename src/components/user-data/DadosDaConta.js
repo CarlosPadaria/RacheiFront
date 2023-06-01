16f0e5124c8409f4c31476a443420b65052fd7a0
Felipe Araujo <felipesc.araujo@gmail.com>
2023-06-01 14:53:28 -0300

    first

--- src/components/user-data/DadosDaConta.js
+++ src/components/user-data/DadosDaConta.js
@@ -3,7 +3,64 @@ import Header from "../elements/header/Header";
 import style from "./User-data.module.css";
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import ShieldIcon from '@mui/icons-material/Shield';
+import axios from "axios";
+import {useAuth} from "../../AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const DadosDaConta = () => {
+
+  const { user, setUser, isLoading, setIsLoading } = useAuth();
+  const [inputs, setInputs] = useState({
+    nome: "",
+    email: "",
+    cpf: "",
+    senha: "",
+  });
+  
+  // const[mensagemErroCadastro, setMensagemErroCadastro] = useState("");
+
+  // const [mensagensErro, setMensagensErro] = useState({
+  //   nome: {
+  //     mensagem: "",
+  //     deuErro: false,
+  //   },
+  //   email: {
+  //     mensagem: "",
+  //     deuErro: false,
+  //   },
+  //   cpf: {
+  //     mensagem: "",
+  //     deuErro: false,
+  //   },
+  //   senha: {
+  //     mensagem: "",
+  //     deuErro: false,
+  //   },
+  // });
+
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if(isLoading == false){
+  //   //  alert(user)
+  //     if(user != null){
+  //     //  alert("oi")
+  //   //  alert('navegando')
+  //     navigate('/');
+  //       //return <Navigate to="/login"></Navigate>;
+  //     }
+  //   }
+  // }, [user, isLoading]);
+
+
+
+
+
+
+
+
+
   return (
     <div className={style['dados-wrapper']}>
       <Header></Header>
@@ -36,7 +93,7 @@ const DadosDaConta = () => {
             <div className={style["content-items"]}>
                 <h2>Dados da conta</h2> 
                 <label placeholder="Nome de usuário" for="nome">Nome de usuário</label>
-                <input id="nome" name="nome" value={'Roberto'}></input>
+                <input id="nome" name="nome" value={user.nome}></input>
                 <label for="cpf">CPF</label>
                 <input className={style['disabled']} id="cpf" name="cpf" disabled value={'123.456.789.12'}></input>
                 <button className={style['submit']}>Salvar Alterações</button>
